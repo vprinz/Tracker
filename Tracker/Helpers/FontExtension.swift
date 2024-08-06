639b8009d63a840f68dbe72fd5ce240c0441b959
Valerii Pavlikov <valerypavlikov@yandex.ru>
2024-08-06 12:45:49 +0300

    - add image - add colors - add extension for font

---
+++ Tracker/Helpers/FontExtension.swift
@@ -0,0 +1,20 @@
+//
+//  FontExtension.swift
+//  Tracker
+//
+//  Created by Valerie N. Prinz on 06/08/2024.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Font {
+    static var screenHeading = Font.system(size: 36, weight: .bold)
+    static var bigHeadline = Font.system(size: 15, weight: .bold)
+    static var smallHeadline = Font.system(size: 10, weight: .bold)
+    static var featuredNumber = Font.system(size: 18)
+    static var featuredText = Font.system(size: 12)
+    static var regularText = Font.system(size: 10)
+    static var captionText = Font.system(size: 8)
+}
