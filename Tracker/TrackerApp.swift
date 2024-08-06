a93ee2ec367fed8b4b036e1187b4890a083adb4e
Valerii Pavlikov <valerypavlikov@yandex.ru>
2024-08-06 12:35:29 +0300

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Valerie N. Prinz on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
